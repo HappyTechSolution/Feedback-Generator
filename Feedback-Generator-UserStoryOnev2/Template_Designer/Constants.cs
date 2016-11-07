044ac53960467198f67dadd50ffd112e81338322
kumudamorla <kumuda.morla.1997@ieee.org>
2016-11-07 17:03:14 +0000

    Add files via upload

---
+++ Feedback-Generator-UserStoryOnev2/Template_Designer/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Designer
+{
+    class Constants
+    {
+        public static String Insert = "INSERT INTO createTemplate(templateName, templateReviewer, templatePosition) VALUES(@templateNameOne, @templateReviewerOne, @templatePositionOne)";
+        public static String InsertOption = "INSERT INTO createTemplateOptions(optionTitle, optionComment) VALUES(@optionTitleOne, @optionCommentOne)";
+    }
+}
---
+++ Feedback-Generator-UserStoryOnev2/Template_Designer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Template_Designer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Set the Database Connection String
+            DBConnection.ConnectionStr = Properties.Settings.Default.feedbackGeneratorDBConnection;
+ //           DBConnection.ConnectionStr = Properties.Settings.Default.myConn;
+
+            //            DBConnection initialise = new DBConnection();
+            //            initialise.openConnection();
+
+            //Opens User Interface.
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TemplateSelector());
+
+
+        }
+    }
+}
